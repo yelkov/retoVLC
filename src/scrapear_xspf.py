fff6413066553b926d3b131672ca78c6a97719ea
YelkoVeiga <yelkovq@gmail.com>
2023-12-04 10:17:42 +0100

    refactor(scrapear_xspf.py):refactorizar y mejorar comentarios

--- src/scrapear_xspf.py
+++ src/scrapear_xspf.py
@@ -1,21 +1,21 @@
-import xml.etree.cElementTree as ET
+import xml.etree.ElementTree as ET
 import os 
 
 def scrapear_xspf(archivo_xspf):
     # Precondición: Verificar si la entrada es un archivo XSPF
     assert archivo_xspf.endswith('.xspf'), f"El archivo {archivo_xspf} no tiene la extensión .xspf."
 
-    #ruta al archivo xspf
+    # Ruta al archivo xspf que usaremos para parsear. Necesitamos introducirlo antes como parametro
     ruta_xspf = os.path.join(os.path.dirname(__file__), '..', 'lista_canciones', archivo_xspf)
     
-    #parsear xspf usando ET
+    # Parsear xspf usando ET y obtenemos raiz, que utilizaremos despues para crear nuestra estructura de datos, que sera una lista
     arbol = ET.parse(ruta_xspf)
     raiz = arbol.getroot()
 
-    #definir espacio de nombres 
+    # Definimos espacio de nombres para iterar entre las etiquetas del xspf
     ns = {'xspf': 'http://xspf.org/ns/0/'}
 
-    #inicializar lista que sera devuelta e iterar sobre los elementos a scrapear
+    # Inicializamos la lista que sera devuelta e iteramos sobre los elementos a scrapear
     lista_localizaciones = []
     for lista in raiz.findall('xspf:trackList', ns):
         for cancion in lista.findall('xspf:track', ns):
@@ -23,6 +23,6 @@ def scrapear_xspf(archivo_xspf):
             lista_localizaciones.append(localizacion)
     
     # Postcondición: Verificar que la lista de localizaciones de canciones es una lista de cadenas
-    assert all(isinstance(localizacion, str) for localizacion in lista_localizaciones), "La lista de localizaciones no contiene cadenas."
+    assert all(isinstance(localizacion, str) for localizacion in lista_localizaciones), "La lista de localizaciones no contiene strings."
 
     return lista_localizaciones
