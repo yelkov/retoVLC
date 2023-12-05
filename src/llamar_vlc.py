1018d910ec52dad3018dd0d8eca2021851ecc909
YelkoVeiga <yelkovq@gmail.com>
2023-12-05 14:00:13 +0100

    feat(llamar_vlc.py): crear modulo que llame vlc

---
+++ src/llamar_vlc.py
@@ -0,0 +1,10 @@
+import subprocess
+
+def llamar_vlc(ruta_vlc,lista_reproduccion):
+
+    # Precondicion: Verificamos que las rutas no están vacías
+    assert ruta_vlc != "", 'La ruta a VLC está vacía.'
+    assert lista_reproduccion != [], 'La ruta a la lista de reproducción está vacía.'
+
+    comando = [ruta_vlc,lista_reproduccion]
+    subprocess.run(comando)
