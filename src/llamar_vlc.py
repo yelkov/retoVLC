4805270e66935fbd3185b69d9759d115bd5ff513
Yelko Veiga <yelkovq@gmail.com>
2023-12-06 19:43:08 +0100

    feat(llamar_vlc.py): probar diferentes os

--- src/llamar_vlc.py
+++ src/llamar_vlc.py
@@ -1,11 +1,49 @@
 import subprocess
-import os
+import platform
+import os 
 
-def llamar_vlc(ruta_vlc,lista_reproduccion):
+def llamar_vlc(lista_reproduccion):
 
-    # Precondicion: Verificamos que las rutas no están vacías
-    assert ruta_vlc != "", 'La ruta a VLC está vacía.'
-    assert lista_reproduccion != [], 'La ruta a la lista de reproducción está vacía.'
+    # Precondicion: Verificamos que la entrada de datos es una lista
+    assert isinstance(lista_reproduccion,list), f'El archivo introducido como parámetro no es una lista.'
 
-    comando = [ruta_vlc] + [lista_reproduccion]
-    subprocess.run(comando)
+    sistema_operativo = platform.system()
+
+    if sistema_operativo == "Windows":
+        try:
+            ruta_vlc = r"C:\\Program Files (x86)\\VideoLAN\\VLC\\vlc.exe"
+            comando = [ruta_vlc] + lista_reproduccion
+            subprocess.run(comando)
+
+        except FileNotFoundError:
+            try:
+                ruta_vlc = r"C:\\Program Files\\VideoLAN\\VLC\\vlc.exe"
+                comando = [ruta_vlc] + lista_reproduccion
+                subprocess.run(comando)
+
+            except FileNotFoundError:
+                "VLC no se encuentra en la ruta esperada."
+
+    elif sistema_operativo == 'Linux':
+        try:
+            ruta_vlc = r"/usr/bin/vlc"
+            comando = [ruta_vlc] + lista_reproduccion
+            subprocess.run(comando)
+
+        except FileNotFoundError:
+            try:
+                ruta_vlc = r"/snap/bin/vlc"
+                comando = [ruta_vlc] + lista_reproduccion
+                subprocess.run(comando)
+
+            except FileNotFoundError:
+                "VLC no se encuentra en la ruta esperada."
+
+    elif sistema_operativo == "Darwin":
+        try:
+            ruta_vlc = r"/Applications/VLC.app/Contents/MacOS/VLC"
+            comando = [ruta_vlc] + lista_reproduccion
+            subprocess.run(comando)
+
+        except FileNotFoundError:
+            "VLC no se encuentra en la ruta esperada."
