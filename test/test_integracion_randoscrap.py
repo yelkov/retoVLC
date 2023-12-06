8e37e41f1158df5305efe594171eb65fdf4755cc
Yelko Veiga <yelkovq@gmail.com>
2023-12-06 19:44:29 +0100

    test(integracion): añadir test integracion

---
+++ test/test_integracion_randoscrap.py
@@ -0,0 +1,29 @@
+from src.parsear_xspf import obtener_localizaciones
+from src.randomizar_lista import randomizar_lista
+
+def test_integracion_longitud():
+    lista_localizaciones_prueba = obtener_localizaciones('lista_prueba.xspf')
+    lista_randomizada_prueba = randomizar_lista(lista_localizaciones_prueba)
+    assert len(lista_localizaciones_prueba) == len(lista_randomizada_prueba)
+
+    lista_localizaciones_creedence = obtener_localizaciones('creedence.xspf')
+    lista_randomizada_creedence = randomizar_lista(lista_localizaciones_creedence)
+    assert len(lista_localizaciones_creedence) == len(lista_randomizada_creedence)
+
+def test_integracion_num_elemetos():
+    lista_localizaciones_prueba = obtener_localizaciones('lista_prueba.xspf')
+    lista_randomizada_prueba = randomizar_lista(lista_localizaciones_prueba)
+    assert len(lista_randomizada_prueba) == 5
+
+    lista_localizaciones_creedence = obtener_localizaciones('creedence.xspf')
+    lista_randomizada_creedence = randomizar_lista(lista_localizaciones_creedence)
+    assert len(lista_localizaciones_creedence) == 25
+
+def test_integracion_diferentes():
+    lista_localizaciones_prueba = obtener_localizaciones('lista_prueba.xspf')
+    lista_randomizada_prueba = randomizar_lista(lista_localizaciones_prueba)
+    assert lista_localizaciones_prueba != lista_randomizada_prueba
+
+    lista_localizaciones_creedence = obtener_localizaciones('creedence.xspf')
+    lista_randomizada_creedence = randomizar_lista(lista_localizaciones_creedence)
+    assert lista_localizaciones_creedence != lista_randomizada_creedence
