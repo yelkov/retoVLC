4c967cb60f20df8c5dec3eeed9bef0933caedc77
Yelko Veiga <yelkovq@gmail.com>
2023-12-06 19:35:36 +0100

    feat(parsear_xspf): añadir try except añadir un bloque try except para controlar si la entrada de datos es un xspf erroneo. Además, cambiamos el nombre al archivo por parsear_xspf y añadimos test para verificar el bloque try except

---
+++ src/parsear_xspf.py
@@ -0,0 +1,34 @@
+import xml.etree.ElementTree as ET
+import os 
+
+def obtener_localizaciones(archivo_xspf):
+    # Precondición: Verificar si la entrada es un archivo XSPF
+    assert archivo_xspf.endswith('.xspf'), f"El archivo {archivo_xspf} no tiene la extensión .xspf."
+
+    # Introducimos un bloque try except para el caso de que el .xspf esté vacío o corrompido
+    try:
+        # Ruta al archivo xspf que usaremos para parsear. Necesitamos introducirlo antes como parametro
+        # pero siempre lo va a buscar en la carpeta 'lista_canciones'
+        ruta_xspf = os.path.join(os.path.dirname(__file__), '..', 'lista_canciones', archivo_xspf)
+        
+        # Parsear xspf usando ET y obtenemos raiz, que utilizaremos despues para crear nuestra estructura de datos, que sera una lista
+        arbol = ET.parse(ruta_xspf)
+        raiz = arbol.getroot()
+
+        # Definimos espacio de nombres para iterar entre las etiquetas del xspf
+        ns = {'xspf': 'http://xspf.org/ns/0/'}
+
+        # Inicializamos la lista que sera devuelta e iteramos sobre los elementos a scrapear
+        lista_localizaciones = []
+        for lista in raiz.findall('xspf:trackList', ns):
+            for cancion in lista.findall('xspf:track', ns):
+                localizacion = cancion.find('xspf:location', ns).text
+                lista_localizaciones.append(localizacion)
+        
+        # Postcondición: Verificamos que la lista de localizaciones de canciones es una lista de cadenas
+        assert all(isinstance(localizacion, str) for localizacion in lista_localizaciones), "La lista de localizaciones no contiene strings."
+
+        return lista_localizaciones
+    
+    except ET.ParseError:
+        raise ET.ParseError("El archivo .xspf introducido está vacío o es erróneo")
