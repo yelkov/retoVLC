5531a5fa97547e288a4cc6de9cae6da3accc49e7
Yelko Veiga <yelkovq@gmail.com>
2023-12-07 23:49:51 +0100

    feat(app.py): añadir validador a app

--- app.py
+++ app.py
@@ -1,23 +1,15 @@
 import sys
 import xml.etree.ElementTree as ET
-from src.validar_xspf import validar_xspf
 from src.parsear_xspf import obtener_localizaciones
+from src.validar_lista_localizaciones import validar_lista_localizaciones
 from src.randomizar_lista import randomizar_lista
 from src.llamar_vlc import llamar_vlc
 
 def reproductor_random(archivo_xspf):
-    try:
-        validar_xspf(archivo_xspf)
-        lista_localizaciones = obtener_localizaciones(archivo_xspf)
-        lista_randomizada = randomizar_lista(lista_localizaciones)
-        llamar_vlc(lista_randomizada)
-
-    except FileNotFoundError:
-        print("No se encuentra el archivo .xspf en la carpeta lista_prueba.")
-    except ValueError:
-        print("El archivo .xspf contiene localizaciones vacías.")
-    except ET.ParseError:
-        print("El archivo está vacío o no sigue correctamente el formato .xspf.")
+    lista_localizaciones = obtener_localizaciones(archivo_xspf)
+    validar_lista_localizaciones(lista_localizaciones)
+    lista_randomizada = randomizar_lista(lista_localizaciones)
+    llamar_vlc(lista_randomizada)
 
 
 if __name__ == '__main__' :
