e99858bac1b09f8cae11a135e938410e75276d2d
Yelko Veiga <yelkovq@gmail.com>
2023-12-07 21:40:23 +0100

    refactor(randomizar_lista.py): cambiar postcond

--- src/randomizar_lista.py
+++ src/randomizar_lista.py
@@ -10,13 +10,8 @@ def randomizar_lista(lista):
     # Utilizamos el metodo shuffle de la libreia random para cambiar de orden los elementos de la lista generada
     random.shuffle(lista_random)
 
-    # Poscondicion: para verificar que todos los elementos de nuestro input se encuentran en la segunda lista, 
-    # vamos a crear dos diccionarios que tengan como claves los elementos de cada una de las listas. 
-    # A ambos diccionarios les vamos a añadir como valor de clave 0, así a pesar de que el orden de los elementos sea diferente, 
-    # si comparten exactamente los mismo elementos con el mismo valor los diccionarios tendran que ser iguales.
-    diccionario_lista = {elemento: 0 for elemento in lista}
-    diccionario_lista_random = {elemento: 0 for elemento in lista_random}
-
-    assert diccionario_lista == diccionario_lista_random, "Los elementos de la lista de canciones no coinciden con los elementos de la lista randomizada"
+    # Poscondicion: para verificar que todos los elementos de nuestro input se encuentran en la segunda lista,
+    # las ordenamos siguiendo el mismo criterio con la funcion sorted 
+    assert sorted(lista_random) == sorted(lista), "La dos listas no contienen los mismos elementos"
 
     return lista_random
