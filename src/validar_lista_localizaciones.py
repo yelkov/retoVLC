d84129f531830d02f9ac94acb181dfc2799955ab
yelkov <yelkovq@gmail.com>
2024-06-18 15:12:27 +0200

    feat(validar_lista_localizaciones.py): añadir mensaje de advertencia

--- src/validar_lista_localizaciones.py
+++ src/validar_lista_localizaciones.py
@@ -6,12 +6,13 @@ def validar_lista_localizaciones(lista_localizaciones):
     assert isinstance(lista_localizaciones,list), "El dato introducido no es una lista."
     
 
-    # Iteramos sobre todos los elementos para comprobar que ninguno está vacío.
+    # Iteramos sobre todos los elementos para comprobar cuantos estan vacíos.
+    contador = 0
     for localizacion in lista_localizaciones:
         if localizacion == None or localizacion.strip() == "" : 
-            messagebox.showinfo("Atención","El archivo .xspf contiene localizaciones vacías.")
-        else:
-            continue
+            contador += 1
+    if contador > 0:
+        messagebox.showinfo("Atención",f"El archivo .xspf contiene {contador} localizaciones vacías de un total de {len(lista_localizaciones)}.")
     
     # Postcondición: Verificamos que la lista de localizaciones de canciones es una lista de cadenas
     assert all(isinstance(localizacion, str) for localizacion in lista_localizaciones), "La lista de localizaciones no contiene strings."
